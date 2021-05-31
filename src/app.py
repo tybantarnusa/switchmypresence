d45a4b5cc1c497d98156b0e9f9d4b09118e6a3fa
Thoyib Antarnusa <tybantarnusa@null.net>
2021-05-31 22:28:22 +0700

    Remove details

--- src/app.py
+++ src/app.py
@@ -2,11 +2,10 @@ from logic.discord import Discord
 
 def main():
 
-    publisher = "Square Enix"
     game = "Bravely Default II"
     image = "bd2"
 
-    discord = Discord(publisher, game, image)
+    discord = Discord(game, image)
     discord.present()
 
 if __name__ == '__main__':
--- src/logic/discord.py
+++ src/logic/discord.py
@@ -4,23 +4,22 @@ import time
 import os
 
 class Discord:
-    def __init__(self, state, details, large_image):
+    def __init__(self, state, large_image):
         load_dotenv()
 
         self.client_id = os.environ.get("CLIENT_ID")
         self.RPC = Presence(self.client_id)
 
         self.state = state
-        self.details = details
         self.large_image = large_image
-        self.large_text = details
+        self.large_text = state
 
     def present(self):
         self.RPC.connect()
         
         start_time = int(time.time())
         
-        self.RPC.update(state=self.state, details=self.details, large_image=self.large_image, large_text=self.large_text, start=start_time)
+        self.RPC.update(state=self.state, large_image=self.large_image, large_text=self.large_text, start=start_time)
         while True:
             time.sleep(15)
     
